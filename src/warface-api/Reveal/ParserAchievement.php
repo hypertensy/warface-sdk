1dc8fd0849e64238bd0e3ab4a3142d330f56ecbe
wnull <x4vh@yandex.ru>
2020-07-27 14:19:31 +0300

    Add method saveImage()

--- src/warface-api/Reveal/ParserAchievement.php
+++ src/warface-api/Reveal/ParserAchievement.php
@@ -62,4 +62,19 @@ class ParserAchievement
 
         return $resource;
     }
+
+    /**
+     * @param string $url
+     * @param string $file
+     */
+    public function saveImage(string $url, string $file): void
+    {
+        $ch = curl_init($url);
+        $fp = fopen($file, 'wb');
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_exec($ch);
+        curl_close($ch);
+        fclose($fp);
+    }
 }

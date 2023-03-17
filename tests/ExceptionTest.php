da4aa1ce95fa8492345d6c870f00f4e5b3914286
wnull <wnullx@yandex.com>
2023-03-17 21:32:58 +0300

    feat/fix: add unit test and fix logics

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Wnull\Warface\Tests;
 
 use Wnull\Warface\Client;
+use Wnull\Warface\Exception\BadRequestException;
 use Wnull\Warface\Exception\InvalidApiEndpointException;
+use Wnull\Warface\Exception\WarfaceApiException;
+use function it;
 
 it(
     'throws invalid api endpoint exception',
@@ -15,4 +18,14 @@ it(
 )->throws(
     InvalidApiEndpointException::class,
     'Call unknown entity'
+);
+
+it(
+    'throws bad request user not found',
+    function () {
+        (new Client())->user()->stat('');
+    },
+)->throws(
+    BadRequestException::class,
+    'Пользователь не найден'
 );
